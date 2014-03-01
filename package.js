afce6a53368c4f35f3bd82d92572312db68ecbb7
Ryan Glover <ryan@wellroundedgent.com>
2014-03-01 13:02:34 -0600

    Forgot to include a few files in the lib.

--- package.js
+++ package.js
@@ -5,7 +5,9 @@ Package.describe({
 Package.on_use(function(api){  
   api.use('jquery', 'client');
   api.add_files([
+  	'lib/jquery-validation/src/ajax.js',
     'lib/jquery-validation/src/core.js',
+    'lib/jquery-validation/src/delegate.js'
   ], 'client');
   api.export('jqueryValidation');
 });
